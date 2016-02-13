b8f7ae8405c4881cf6bbd48bd50ea3237a463231
atowneND <atowne@nd.edu>
2016-02-13 14:57:21 -0500

    finished d9

--- python_dailies/d9/d9.py
+++ python_dailies/d9/d9.py
@@ -12,22 +12,43 @@ department = []
 salary = []
 
 with open(inputfile) as f:
+    # part 1
     data = csv.DictReader(f)
-    total_salaries = 0
-    num_employees = 0
     for row in data:
         if (row['Name']):
             name.append(row['Name'])
             position.append(row['Position Title'])
             department.append(row['Department'])
             salary.append(row['Employee Annual Salary'])
+
+    # part 2
     for i in xrange(len(salary)):
-        s = float(salary[i].replace('$',''))
-        if (s!=0):
+        salary[i] = float(salary[i].replace('$',''))
+
+    # part 3
+    total_salaries = 0
+    num_employees = 0
+    for i in xrange(len(salary)):
+        if (salary[i]!=0):
             num_employees = num_employees +1
-        total_salaries = total_salaries + s
+        total_salaries = total_salaries + salary[i]
+
     maxnumlen = len(str(total_salaries))
     avg_salary = total_salaries/num_employees
-    print 'Total payroll:       ${0}'.format(total_salaries)
-    print 'Number of employees:  {0}{1}'.format(' '*(maxnumlen-len(str(num_employees))),num_employees)
-    print 'Average yearly pay:  ${:>10}'.format(' '*(maxnumlen-len(str(avg_salary))),avg_salary)
+
+    # THIS FORMAT DOESN'T WORK ON THE PYTHON VERSION THAT THE STUDENT MACHINES
+    # HAVE BUT IT WORKS ON MY LAPTOP
+#    print 'Total payroll:       ${:>}'.format(total_salaries)
+#    print 'Number of employees: {:>{}}'.format(num_employees,maxnumlen+1)
+#    print 'Average yearly pay:  ${:>{}.>2f}'.format(avg_salary,maxnumlen)
+
+    print 'Total payroll:       $%0.2f' % total_salaries
+    print "Number of employees:  %s%i" % (" "*(maxnumlen-len(str(num_employees))), num_employees)
+    print "Average yearly pay:  $%s%0.2f" % (" "*(maxnumlen-len(str(round(avg_salary,2)))), avg_salary)
+
+    # part 4
+    department_set = set(department)
+    print "Number of city departments: %i" % len(department_set)
+    print "Names of city departments: "
+    for i in department_set:
+        print "\"%s\"" % i
