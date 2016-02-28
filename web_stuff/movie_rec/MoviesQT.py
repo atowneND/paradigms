7ef3712112dac9be3ae9f8cbc6365f1c5e5a15aa
atowneND <atowne@nd.edu>
2016-02-28 00:08:55 -0500

    got stuck on adding an image

--- web_stuff/movie_rec/MoviesQT.py
+++ web_stuff/movie_rec/MoviesQT.py
@@ -5,15 +5,47 @@
 # get requests for 2.6 nastiness
 import sys
 sys.path.append('/afs/nd.edu/user37/cmc/Public/paradigms/python/local/lib/python2.6/site-packages/requests-2.0.0-py2.6.egg')
+sys.path.append('/afs/nd.edu/user37/cmc/Public/cse332_sp16/cherrpy/data/images/')
 
 # real code
+from moviedb import _webservice_primer as mdb# from webservice primer
 import PyQt4.QtCore as qtcore
 import PyQt4.QtGui as qtgui
+import requests
+import json
 
 class MoviesQT(qtgui.QMainWindow):
     def __init__(self):
         super(MoviesQT, self).__init__()
+        self.movies = mdb._webservice_primer()
+
         self.setWindowTitle("Movie Recommender")
+        
+        self.central = MoviesCentral(parent = self)
+        self.setCentralWidget(self.central)
+
+        self.uid = 5
+        mid = self.getMID()
+        img = self.getMoviePoster(mid)
+
+        pixmap = qtgui.QPixmap(str(img))
+        movie_pic = qtgui.QLabel(self)
+        movie_pic.setPixmap(pixmap)
+
+    def getMID(self):
+        r = requests.get(self.movies.RECOMMENDATIONS_URL + str(self.uid))
+        return r.json()['movie_id']
+        
+    def getMoviePoster(self, mid):
+        r = requests.get(self.movies.MOVIES_URL + str(mid))
+        return r.json()['img']
+
+class MoviesCentral(qtgui.QWidget):
+    def __init__(self, parent=None):
+        super(MoviesCentral, self).__init__(parent)
 
 if __name__ == "__main__":
     app = qtgui.QApplication(sys.argv)
+    gui = MoviesQT()
+    gui.show()
+    sys.exit(app.exec_())
---
+++ web_stuff/movie_rec/moviedb/_webservice_primer.py
@@ -0,0 +1,39 @@
+# Ashley Towne
+# 2/26/2016
+# web service primer
+
+# get requests for 2.6 nastiness
+import sys
+sys.path.append('/afs/nd.edu/user37/cmc/Public/paradigms/python/local/lib/python2.6/site-packages/requests-2.0.0-py2.6.egg')
+
+# real code
+import requests
+import json
+
+class _webservice_primer:
+    def __init__(self):
+        self.SITE_URL = 'http://student02.cse.nd.edu:40001'
+        self.MOVIES_URL = self.SITE_URL + '/movies/'
+        self.RESET_URL = self.SITE_URL + '/reset/'
+        self.RECOMMENDATIONS_URL = self.SITE_URL + '/recommendations/'
+
+    def get_movie(self, mid):
+        r = requests.get(self.MOVIES_URL + str(mid))
+        return r.json()
+
+    def set_movie_title(self, mid, title):
+        movie = self.get_movie(mid)
+        movie['title'] = 'Something Else'
+        r = requests.put(self.MOVIES_URL + str(mid), data = json.dumps(movie))
+        return r.json()
+
+    def delete_movie(self, mid):
+        mydata = {}
+        r = requests.delete(self.MOVIES_URL + str(mid), data = json.dumps(mydata))
+        return r.json()
+
+    def reset_movie(self, mid):
+        mydata = {}
+        r = requests.put(self.RESET_URL + str(mid), data = json.dumps(mydata))
+        return r.json()
+
