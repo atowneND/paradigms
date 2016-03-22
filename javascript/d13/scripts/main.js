7a40257871ece636712d44d2703f4f007010f013
atowneND <atowne@nd.edu>
2016-03-22 01:40:45 -0400

    finished d13

--- javascript/d13/scripts/main.js
+++ javascript/d13/scripts/main.js
@@ -1,67 +1,42 @@
-var Item = {addToDocument:function(){ document.body.appendChild(this.item); },
+// Ashley Towne
+// 03/23/2016
+// d13
+
+var Item = {addToDocument:function(){
+                document.body.appendChild(this.item); },
            };
 
 var Label = {createLabel:function(text, id){
-                var this.item = document.createElement("p");
+                this.item = document.createElement("p");
                 this.item.setAttribute(id, text);
                 this.addToDocument();},
              setText:function(text){
-                var labelText = document.createTextNode(text);
-                this.item.appendChild(labelText);
+                this.item.textContent = text;
                 this.addToDocument();},
             };
 
 var Button = {createButton:function(text, id){
-                var this.item = document.createElement("button");
+                this.item = document.createElement("button");
                 this.item.setAttribute(id, text);
+                var labelText = document.createTextNode(text);
+                this.item.appendChild(labelText);
                 this.addToDocument();},
               addClickEventHandler:function(handler, args){
-                 this.item.onmouseup.function(){handler(args)};},
+                 this.item.onmouseup = function() { handler(args); };
+                 },
              }
-function addClickEventHandler(args){alert(args[0]):}
+function changeText(args){
+    args[1].setText(args[0]);
+}
 
 Label.__proto__ = Item;
 Button.__proto__ = Item;
 
-Button.createButton("hi","id");
+Button.createButton("CLICK HERE","theButon");
 
-//var Label = document.createElement("l");
-//
-//// from class
-//var label = document.createElement("p");
-//label.setAttribute("id","theLabel");
-//var labelText = document.createTextNode("who?");
-//label.appendChild(labelText);
-//document.body.appendChild(label);
-//function changeText() {label.innerHTML="Patrick Flynn";}
-//var Bird = {name:"Larry",
-//            fly:function(){alert("flap");}}
-//var duck = {name:"donald",
-//            wingspan:"20",
-//            quack:function(){alert("quacking");},
-//            addClickHandler:function(hndlr,args){
-//                document.getElementById("theButton").onmouseup.function(){hndlr(args);}
-//                }
-//            };
-//duck.quack();
-//duck.__proto__ = Bird;
-//function showMyMessage(args){alert(args[0]):}
-//duck.addClickHandler(showMyMessage,["my duck click"]);
-//
-//duck.fly();
-//alert(Bird.name); "Larry"
-//alert(duck.name); "Donald"
-//
-//// old assignment
-//var label = document.createElement("p");
-//label.setAttribute("id","thelabel");
-//var labelText = document.createTextNode("Who?");
-//label.appendChild(labelText);
-//document.body.appendChild(label);
-//
-//var button = document.getElementById("thebutton");
-//
-//function mouseup() {
-//    labelText.textContent="Ashley";
-//}
-//button.onclick = mouseup
+Label.createLabel("guess who", "theLabel");
+Label.setText("who?");
+Label.addToDocument();
+
+args = ["Ashley Towne", Label]
+Button.addClickEventHandler(changeText, args);
