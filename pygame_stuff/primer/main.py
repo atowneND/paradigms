9389121e512808917a2c8015cbe71825d2fbcef1
atowneND <atowne@nd.edu>
2016-04-10 21:01:26 -0400

    finished the lazy version of the assignment

--- pygame_stuff/primer/main.py
+++ pygame_stuff/primer/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import pygame
 import math
 
@@ -38,6 +39,7 @@ class GameSpace:
                 self.player.rect.x  = self.player.rect.x - 1
             if keys[pygame.K_SPACE]:
                 self.player.tofire = True
+                self.enemy.newhit = True
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -51,10 +53,13 @@ class GameSpace:
             # display game objects
             self.screen.fill(self.black)
 
+            if self.enemy.health >= 0:
+                self.screen.blit(self.enemy.image, self.enemy.rect)
+            else:
+                self.enemy.sound.stop()
+            self.screen.blit(self.player.image, self.player.rect)
             if self.laser.fire == True:
                 self.screen.blit(self.laser.image, self.laser.rect)
-            self.screen.blit(self.enemy.image, self.enemy.rect)
-            self.screen.blit(self.player.image, self.player.rect)
 
             pygame.display.flip()
 
@@ -65,6 +70,7 @@ class Player(pygame.sprite.Sprite):
         self.gs = gs
         self.image = pygame.image.load("media/deathstar.png")
         self.rect = self.image.get_rect()
+        self.angle = 0
 
         # keep original image to limit resize errors
         self.orig_image = self.image
@@ -77,21 +83,20 @@ class Player(pygame.sprite.Sprite):
         mx, my = pygame.mouse.get_pos()
 
         # compute angle between current direction and mouse position
-        sight = [self.rect.right, self.rect.bottom - self.rect.height/4.]
-        theta_sight = math.atan2(sight[0]-self.rect.center[0],sight[1]-self.rect.center[1])
-        theta_mouse = math.atan2(mx-self.rect.center[0],my-self.rect.center[1])
-        dtheta = theta_sight + theta_mouse
+        theta_mouse = math.atan2(my-self.rect.center[1],mx-self.rect.center[0])
+        dtheta = (-(theta_mouse + self.angle)*180/math.pi)/2.
+        self.angle = theta_mouse
 
         # prevents movement while firing
         if self.tofire == True:
             self.gs.laser.fire = True
-            self.gs.laser.rect.x = self.rect.right - self.rect.width/4.
-            self.gs.laser.rect.y = self.rect.center[1] - self.rect.height/4.
-            self.gs.laser.dtheta = dtheta
+            self.gs.laser.rect.x = self.rect.center[0] + math.cos(self.angle - math.pi/2)*self.rect.width/4.
+            self.gs.laser.rect.y = self.rect.center[1] + math.sin(self.angle - math.pi/2)*self.rect.height/4.
+            self.gs.laser.dtheta = self.angle - math.pi/2
             self.tofire = False
         else:
             # rotate the image to face the mouse
-            self.image = pygame.transform.rotate(self.orig_image, 180+dtheta*180/math.pi)
+            self.image = pygame.transform.rotate(self.orig_image, dtheta)
             self.rect = self.image.get_rect(center=self.rect.center)
 
 class Enemy(pygame.sprite.Sprite):
@@ -101,15 +106,25 @@ class Enemy(pygame.sprite.Sprite):
         self.gs = gs
         self.image = pygame.image.load("media/globe.png")
         self.rect = self.image.get_rect()
-        self.rect.x = self.gs.width/2.
-        self.rect.y = self.gs.height/2.
+
+        self.sound = pygame.mixer.Sound("media/explode.wav")
 
         # properties
-        self.health = 10
+        self.health = 5
+        self.rect.x = self.gs.width/2.
+        self.rect.y = self.gs.height/2.
+        self.radius = self.rect.width/2.
+        self.newhit = True
 
     def tick(self):
-        # get mouse x and y position on the screen
-        mx, my = pygame.mouse.get_pos()
+        if pygame.sprite.collide_circle(self, self.gs.laser) and self.newhit==True:
+            self.health -= 1
+            self.newhit = False
+            self.gs.laser.fire = False
+            self.gs.laser.sound.stop()
+            if self.health == 0:
+                self.image = pygame.image.load("media/globe_red100.png")
+                self.sound.play()
 
 class Weapon(pygame.sprite.Sprite):
     def __init__(self,gs=None):
@@ -119,11 +134,14 @@ class Weapon(pygame.sprite.Sprite):
         self.image = pygame.image.load("media/laser.png")
         self.rect = self.image.get_rect()
         self.fire = False
-        self.speed = 3
+        self.speed = 6
+
+        self.sound = pygame.mixer.Sound("media/screammachine.wav")
 
     def tick(self):
-        self.rect.x += self.speed*math.cos(math.pi*3/4 - self.dtheta)
-        self.rect.y += self.speed*math.sin(math.pi*3/4 - self.dtheta)
+        self.rect.x += self.speed*math.cos(self.dtheta+math.pi/2)
+        self.rect.y += self.speed*math.sin(self.dtheta+math.pi/2)
+        self.sound.play()
 
 if __name__ == '__main__':
     gs = GameSpace()
