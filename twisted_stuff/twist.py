c1206b331ea9338bb721e7a2505d0c6ac2a6da3e
atowneND <atowne@nd.edu>
2016-04-17 17:46:45 -0400

    finished twisted 1 daily 16

---
+++ twisted_stuff/twist.py
@@ -0,0 +1,49 @@
+from twisted.internet import reactor as reactor
+from twisted.internet.protocol import ClientFactory, Protocol
+
+host = "student02.cse.nd.edu"
+port = 40001
+
+class ClientConnection(Protocol):
+    """
+    Client Connection: for a single connection/request
+    """
+    def dataReceived(self, data):
+        """
+        This is called when the server sends data back to the client. It
+        prints it out for the user to see.
+        """
+        print "received:", data
+
+    def connectionMade(self):
+        """
+        This is called when the connection is made and executes the get request
+        """
+        print "made connection"
+        self.transport.write("GET /movies/32 HTTP/1.0\r\n\r\n") # some get request
+
+    def connectionLost(self, reason):
+        """
+        This is called when the connection is lost, for whatever reason. Even
+        if the connection is terminated cleanly, it will state the reason and
+        stop the reactor
+        """
+        print "connection lost because", reason
+        reactor.stop()
+
+class ClientConnectionFactory(ClientFactory):
+    """
+    Factory to handle any number of instances of connections
+    """
+    protocol = ClientConnection
+    def buildProtocol(self, addr):
+        """
+        Generates the protocol upon connection to spin up a ClientConnection
+        instance to handle the request
+        """
+        print "Connected"
+        return ClientConnection()
+
+if __name__ == "__main__":
+    reactor.connectTCP(host, port, ClientConnectionFactory())
+    reactor.run()
